8ab7a1a95844c5b3a32227954fe65f05fbff179b
Ini Etienam <ietienam@gmail.com>
2020-05-11 19:04:25 +0100

    tennis tournament challenge test passes

--- TennisTournament/TennisTournamentSolution.js
+++ TennisTournament/TennisTournamentSolution.js
@@ -1,6 +1,19 @@
 function solution(P, C) {
-    //Provide Your solution here 
-    
+    //Provide Your solution here
+    var players = P;
+    var courts = C;
+    var numOfGames = 0;
+    if (players % 2 !== 0) {
+      players -= 1;
+    }
+    for (var i = players; i > 0; i -= 2) {
+      if (courts === 0) {
+        break;
+      }
+      numOfGames += 1;
+      courts -= 1;
+    }
+    return numOfGames;
 }
 
 module.exports = solution;
